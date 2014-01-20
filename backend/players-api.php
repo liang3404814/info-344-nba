9037fed1a6eaa02d476d8d0a6f72d78b40192e74
liang3404814 <liang3404814@gmail.com>
2014-01-19 21:44:37 -0800

    first major version...

--- backend/players-api.php
+++ backend/players-api.php
@@ -6,22 +6,111 @@ include_once "players-db-access.php";
 ini_set('display_errors', 1);
 error_reporting(~0);
 
-get_players_list();
+$supported_actions = array("get_players", "get_player");
 
+if (isset($_GET["action"]) && in_array($_GET["action"], $supported_actions)) {
+	switch ($_GET["action"]) {
+		case 'get_players':
+			get_players_list();
+			break;
 
+		case 'get_player':
+			if (isset($_GET["id"])) {
+				get_player_by_id($_GET["id"]);
+			} else {
+				error("missing_argument");
+			}
+			break;
+		
+		default:
+			error("unknown_action");
+			break;
+	}
+}
 
-// function for returning list of first names and last names
-// of all players
+// Error handling part
 
-$nba_db = new Database;
-
-
-function get_players_list() {
-	$nba_db->run_query("SELECT `PlayerName` FROM `nba_players`");
-	$nba_db->list_from_field("PlayerName");
+function error($type) {
+	echo json_encode(array("status" => "error_$type"));
 }
 
 
+// Returns a JSON that has all first names and last names of NBA players that we have.
+// player ID, First and Last Names.
+function get_players_list() {
 
+	$result;
 
- ?>
+	$nba_db = new Database();
+	$nba_db->db_connect();
+
+	$query = "SELECT `PlayerName` as `fullName`, `id` FROM `nba_players`";
+
+	$nba_db->run_query($query);
+
+	$result;
+	$status;
+	
+	if ($nba_db->sql_error_code() == 0) {
+
+		$db_result = $nba_db->all_results_in_array();
+
+		// splits names so we have bot first and last names in the return
+		foreach ($db_result as $key => $value) {
+			$names = explode(" ", $db_result[$key]['fullName']);
+			$firstName = $names[0];
+			$lastName = $names[1];
+			//unset($db_result[$key]['PlayerName']);
+			$db_result[$key]['firstName'] = $firstName;
+			$db_result[$key]['lastName'] = $lastName;
+		}
+
+		$result = array("status" => "OK", "content" => $db_result);
+	} else {
+		$status = array(
+			"status" => "error",
+			"error_code" => $nba_db->sql_error_code()
+			);
+		$result = $status;
+	}
+
+	
+
+	echo json_encode($result);
+
+}
+
+// Returns a player's all info wrapped in JSON format.
+// player ID and everything
+function get_player_by_id($id) {
+	$result;
+
+	$nba_db = new Database();
+	$nba_db->db_connect();
+
+	$query = "SELECT `PlayerName` as `fullName`,
+	 `id`, 
+	 `GP`,
+	 `FGP`,
+	 `TPP`,
+	 `FTP`,
+	 `PPG`  
+	 FROM `nba_players` WHERE id = ?";
+
+	$nba_db->run_secure_query($query, array($id));
+	$result = $nba_db->all_results_in_array();
+
+	// splits names so we have bot first and last names in the return
+	// foreach ($result as $key => $value) {
+	// 	$names = explode(" ", $result[$key]['PlayerName']);
+	// 	$firstName = $names[0];
+	// 	$lastName = $names[1];
+	// 	unset($result[$key]['PlayerName']);
+	// 	$result[$key]['firstName'] = $firstName;
+	// 	$result[$key]['lastName'] = $lastName;
+	// }
+
+	echo json_encode($result);
+}
+
+?>
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,175 @@
-var nbaApp = angular./**
+var nbaApp = /**
 * nbaApp Module
 *
 * Description
 */
-angular.module('nbaApp', ['ui.bootstrap']);
+angular.module('nbaApp', ['ui.router', 'ui.bootstrap', 'ngProgressLite']);
+
+nbaApp.directive('nbaPlayerHeadImage', function() {
+
+	return {
+		restrict: 'E', 
+		// require: '^ngNBAPlayer',
+		scope: {
+			playerName: '@playerName'
+		},
+		template: '<img ng-src="{{headPictureURL}}" alt="profile picture for {{playerName}}" />',
+		link: function($scope) {
+			var url = "http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/";
+			var name = $scope.playerName.toLowerCase().split(' ').join('_');
+			$scope.headPictureURL = url + name + '.png';
+		}
+	};
+});
+
+nbaApp.directive('nbaPlayerNewsImage', function(){
+	// Runs during compile
+	
+	return {
+		restrict: 'E',
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		scope: {
+			playerName: '@playerName'
+		},
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		controller: function($scope, $http) {
+			var name = $scope.playerName.toLowerCase().split(' ').join('+');
+			// var url = "http://searchapp2.nba.com/nba-search/query.jsp?text=" + name + "&type=playerpage&start=1&npp=10&site=nba&hide=true&sort=relevance&output=json";
+			var url = "http://searchapp2.nba.com/nba-search/query.jsp";
+			$http.jsonp(url, {
+				params: {
+				"text": name, 
+				"type": "playerpage",
+				"start": 1,
+				"npp": 10,
+				"site": "nba",
+				"hide": "true",
+				"sort": "relevance",
+				"output": "jsonp" }
+			});
+
+			// Yes, the NBA api doens't respond to callback function name settings...
+			// can't complain since it's an internal API.
+			// and Yes, Chrome will complain "Resource interpreted as Script but transferred with MIME type text/html",
+			// NBA's backend guys need to do some work!
+			window.nbaProcessSearchResults = function(data) {
+				var results = data["results"][0];
+				console.log(results);
+
+				for (var i = 0; i < results.length; i++) {
+					results[i] = results[i]["metadata"];
+					results[i] = results[i]['media'];
+				};
+
+				console.log(results);
+
+				$scope.playerNewsImages = results;
+			}
+
+		},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		templateUrl: 'templates/player-news-images.html',
+		// templateUrl: '',
+		replace: true
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+	};
+});
+
+
+nbaApp.config(function($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	.state('start', {
+		url: "/",
+		templateUrl: 'partials/start.html'
+	})
+
+	.state('playerDetailView', {
+		url: "/players/:playerId",
+		templateUrl: 'partials/player.detail.html',
+		resolve: {
+			playerDetails: function($http, $stateParams) {
+				var playerId = $stateParams.playerId;
+				return $http.get('../backend/players-api.php?action=get_player&id=' + playerId);
+				// return $http.get('../backend/players-api.php', {
+				// 	params: {"action": "get_player", "id": playerID
+				// 	}
+				// });
+}
+},
+controller: function($scope, $http, playerDetails) {
+	$scope.playerDetails = playerDetails.data[0];
+}
+})
+});
+
+nbaApp.config(['ngProgressLiteProvider', function(ngProgressLiteProvider) {
+	//ngProgressLiteProvider.settings.speed = 1500;
+}]);
+
+nbaApp.run(function($rootScope, ngProgressLite, $timeout) {
+	$rootScope.$on('$stateChangeStart', function() {
+		ngProgressLite.start();
+		
+	})
+
+	$rootScope.$on('$stateChangeSuccess', function() {
+		ngProgressLite.done();
+	})
+});
+
+
+function playerDetailControl () {
+	// pictures: http://searchapp2.nba.com/nba-search/query.jsp?text=tony+allen&type=playerpage&start=1&npp=10&site=nba&hide=true&sort=relevance&output=json
+	// articles: http://searchapp2.nba.com/nba-search/query.jsp?text=%22tony+allen%22&type=article&start=1&npp=50&site=nba&hide=true&sort=relevance&output=json
+}
+
+
+function searchControl ($scope, $http, $state, $location,  $anchorScroll) {
+	
+
+	// $http.get('../backend/players-api.php?action=get_players').success(function(data) {
+		
+	// 	$scope.playerNames = data["content"];
+	// 	// console.log($scope.playerNames);
+	// 	// console.log($scope.fuzzy_matched_players("lebron"));
+	// });
+
+$http.get('../backend/players-api.php', {
+	params: {"action": "get_players"}
+}).success(function(data) {
+	$scope.playerNames = data["content"];
+});
+
+
+$scope.fuzzy_matched_players = function(name) {
+
+	var options = {
+		keys: ['firstName', 'lastName']
+	}
+
+	var f = new Fuse($scope.playerNames, options);
+	var result = f.search(name);
+
+	return result;
+};
+
+$scope.onSelect = function($item, $model, $label) {
+	var params = {
+		playerId: $item.id
+	}
+
+	$state.go('playerDetailView', params);
+
+	$location.hash('main-content');
+	$anchorScroll();
+}
+
+}
